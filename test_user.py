51b4c1fbe54f04802549782fffc1324ab383cf68
Andi Efendi <andiefendi.id@gmail.com>
2025-11-23 17:39:23 +0700

    update data

--- test_user.py
+++ test_user.py
@@ -5,13 +5,20 @@ import jsonpath
 import random
 import os
 from pathlib import Path
-
+import requests
+import allure
 
 
 
 baseUrl = "https://reqres.in/"
 
 #### Example code ####
+
+@allure.feature("Products API")
+@allure.story("Get Product List")
+@allure.title("Get Product List Hepi flow dah")
+@allure.description("Memastikan GET /products mengembalikan daftar produk dengan response 200 dan struktur JSON yang valid.")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_list_user():
     global tokenLogin
    
@@ -19,7 +26,10 @@ def test_list_user():
     headers = {
         'Content-Type':'application/json'
         } 
-    
-    response = requests.get(url=baseUrl+path, headers=headers)
-    assert response.status_code == 200
+    with allure.step(f"Kirim request GET ke {url} pake params {PARAMS}"):
+        response = requests.get(url=baseUrl+path, headers=headers)
+
+    with allure.step("Validasi status code 200"):    
+        assert response.status_code == 200
+        
     print(response.text)
